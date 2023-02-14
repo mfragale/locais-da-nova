ac9d78a0227db7e4bd09c047324da9bf88c6fd2f
Mauricio Fragale <mfragale@gmail.com>
2023-02-13 23:32:28 -0300

    Update add-actions.php

--- includes/add-actions.php
+++ includes/add-actions.php
@@ -14,30 +14,3 @@ add_action('wp_enqueue_scripts', 'locais_da_nova_js', true);
  * Add wp_enqueue_scripts hook for CSS files
  */
 add_action('wp_enqueue_scripts', 'locais_da_nova_css');
-
-
-
-
-/**
- * Registers locais-da-nova-nav
- */
-//add_action('init', 'register_locaisdanova_menu');
-
-
-/**
- * FORMS action callback hooks
- */
-// add_action('admin_post_stripe_checkout', 'stripe_checkout'); // If the user is logged in
-// add_action('admin_post_nopriv_stripe_checkout', 'stripe_checkout'); // If the user in not logged in
-
-
-/**
- * AJAX action callback hooks
- */
-//Load More Charges
-// add_action('wp_ajax_locaisdanova_load_more_charges', 'locaisdanova_load_more_charges_ajax');
-// add_action('wp_ajax_nopriv_locaisdanova_load_more_charges', 'locaisdanova_load_more_charges_ajax');
-
-//Load More Recurring Donations
-// add_action('wp_ajax_locaisdanova_load_more_recurring_donations', 'locaisdanova_load_more_recurring_donations_ajax');
-// add_action('wp_ajax_nopriv_locaisdanova_load_more_recurring_donations', 'locaisdanova_load_more_recurring_donations_ajax');
